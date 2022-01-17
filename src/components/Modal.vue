783f3f85e364b3502c91d5cc0f7cebd8cfbb70de
shafucheng <flynn.sha@outlook.com>
2022-01-17 11:32:04 +0800

    feat: 支持出现纵向滚动条，同时ref改为最新的$ref语法糖

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -14,79 +14,91 @@ const emit = defineEmits<{
   (e: 'update:visible', visible: boolean): void
 }>()
 
-const visible = useVModel(props, 'visible', emit)
+let visible = $(useVModel(props, 'visible', emit))
 
 // 弹窗实例的唯一ID，用于获取dom
 const id = nanoid()
 
 // 是否已经获取数据
-const isReady = ref(false)
+let isReady = $ref(false)
 
 // modal dom 元素
-const modalEl = ref()
-const modalHeaderEl = ref()
+let modalEl = $ref<HTMLElement>()
+let modalSizeEl = $ref<HTMLElement>()
+let modalHeaderEl = $ref<HTMLElement>()
 
 // 用以保证关闭时收缩动画收缩到的位置的准确性
-const originX = ref(0)
-const originY = ref(0)
+let originX = $ref(0)
+let originY = $ref(0)
 
-const { x, y } = useDraggable(modalHeaderEl)
-const { width, height } = useElementSize(modalEl)
-const { width: windowWidth, height: windowHeight } = useWindowSize()
+let { x, y } = $(useDraggable($$(modalHeaderEl)))
+let { width, height } = $(useElementSize($$(modalEl)))
+let { width: windowWidth, height: windowHeight } = $(
+  useElementSize($$(modalSizeEl)),
+)
 
 // 弹窗 css
 const style = computed(() => {
-  if (!isReady.value) {
+  if (!isReady) {
     return {}
   }
   return {
-    left: `${x.value}px`,
-    top: `${y.value}px`,
-    transformOrigin: `${originX.value - x.value}px ${
-      originY.value - y.value
-    }px`,
+    left: `${x}px`,
+    top: `${y}px`,
+    transformOrigin: `${originX - x}px ${originY - y}px`,
   }
 })
 
 // x超出边界修正
 watchEffect(() => {
-  if (x.value < 0) {
-    x.value = 0
-  } else if (x.value + width.value > windowWidth.value) {
-    x.value = windowWidth.value - width.value
+  if (windowWidth > 0) {
+    if (x < 0) {
+      x = 0
+    } else if (windowWidth > 0 && windowWidth - width < x) {
+      const newX = Math.max(windowWidth - width, 0)
+      if (x !== newX) {
+        x = newX
+      }
+    }
   }
 })
 
 // y超出边界修正
 watchEffect(() => {
-  if (y.value < 0) {
-    y.value = 0
-  } else if (y.value + height.value > windowHeight.value) {
-    y.value = windowHeight.value - height.value
+  if (y < 0) {
+    y = 0
+  } else if (windowHeight > 0 && windowHeight - height < y) {
+    const newY = Math.max(windowHeight - height, 0)
+    if (y !== newY) {
+      y = newY
+    }
   }
 })
 
 // 获取dom，每次 visible === true 都要重新获取，因为transform-origin时每次打开时根据鼠标位置变化的
 watchEffect(async () => {
-  if (visible.value === true) {
-    isReady.value = false
+  if (visible === true) {
+    isReady = false
     await nextTick()
-    const modal = document.getElementsByClassName(id)[0] as HTMLElement
-    const header = modal.getElementsByClassName(
+    modalEl = document.getElementsByClassName(id)[0] as HTMLElement
+    if (!modalSizeEl) {
+      modalSizeEl = document.createElement('div')
+      modalSizeEl.style.width = '100%'
+      modalSizeEl.style.height = '100%'
+      modalEl.parentNode?.appendChild(modalSizeEl)
+    }
+    modalHeaderEl = modalEl.getElementsByClassName(
       'ant-modal-header',
     )[0] as HTMLElement
-
-    modalEl.value = modal
-    modalHeaderEl.value = header
-    x.value = modal.offsetLeft
-    y.value = modal.offsetTop
+    x = modalEl.offsetLeft
+    y = modalEl.offsetTop
     await nextTick()
-    const [transformOriginX, transformOriginY] = modal.style.transformOrigin
+    const [transformOriginX, transformOriginY] = modalEl.style.transformOrigin
       .split(' ')
       .map(parseInt)
-    originX.value = x.value + transformOriginX
-    originY.value = y.value + transformOriginY
-    isReady.value = true
+    originX = x + transformOriginX
+    originY = y + transformOriginY
+    isReady = true
   }
 })
 </script>
@@ -95,7 +107,7 @@ watchEffect(async () => {
   <a-modal
     v-model:visible="visible"
     v-bind="attrs"
-    class="vB-gWURJj6yaDCJAqHcLo"
+    wrap-class-name="vB-gWURJj6yaDCJAqHcLo"
     :class="{ draggable: isReady, [id]: true }"
     :style="style"
   >
@@ -105,15 +117,20 @@ watchEffect(async () => {
 
 <style lang="scss">
 .vB-gWURJj6yaDCJAqHcLo {
-  &.draggable {
-    position: absolute;
-    // 清除默认样式
-    padding: 0;
-    // 页面宽度小于768时会多一个margin: 8px auto
-    margin: 0;
-    .ant-modal-header {
-      user-select: none;
-      cursor: move;
+  overflow-x: hidden;
+  .ant-modal {
+    // 默认：max-width: calc(100vw - 32px);
+    max-width: 100%;
+    &.draggable {
+      position: absolute;
+      // 清除默认样式
+      padding: 0;
+      // 页面宽度小于768时会多一个margin: 8px auto
+      margin: 0;
+      .ant-modal-header {
+        user-select: none;
+        cursor: move;
+      }
     }
   }
 }
