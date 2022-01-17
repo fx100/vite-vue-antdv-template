8543276ec734d62fd949404042425ddaa7cb92fd
shafucheng <flynn.sha@outlook.com>
2022-01-17 11:28:21 +0800

    add ant-design-vue@^3

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,6 +13,9 @@ watchEffect(() => {
 </script>
 
 <template>
-  <button type="button" @click="add">count is: {{ count }}</button>
+  <a-button @click="add">
+    <template #icon><ant-design-outlined /></template>
+    count is: {{ count }}
+  </a-button>
   <p>鼠标位置: ({{ x }}, {{ y }})</p>
 </template>
