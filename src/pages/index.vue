718ad8219dafd176710bc2c24c8956626c8151c8
shafucheng <flynn.sha@outlook.com>
2022-01-17 13:26:16 +0800

    Revert "remove: 监听鼠标位置的console.log"
    This reverts commit e6b59bf2b2cd4fd543512de0b88bc6f0b0a738c6.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,6 +9,9 @@ const { x, y } = $(useMouse())
 </script>
 
 <template>
-  <button type="button" @click="add">count is: {{ count }}</button>
+  <a-button @click="add">
+    <template #icon><ant-design-outlined /></template>
+    count is: {{ count }}
+  </a-button>
   <p>鼠标位置: ({{ x }}, {{ y }})</p>
 </template>
