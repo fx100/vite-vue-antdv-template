e6b59bf2b2cd4fd543512de0b88bc6f0b0a738c6
shafucheng <flynn.sha@outlook.com>
2022-01-17 11:36:08 +0800

    remove: 监听鼠标位置的console.log

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,16 +6,9 @@ function add() {
 }
 
 const { x, y } = $(useMouse())
-
-watchEffect(() => {
-  console.log(`鼠标位置: (${x}, ${y})`)
-})
 </script>
 
 <template>
-  <a-button @click="add">
-    <template #icon><ant-design-outlined /></template>
-    count is: {{ count }}
-  </a-button>
+  <button type="button" @click="add">count is: {{ count }}</button>
   <p>鼠标位置: ({{ x }}, {{ y }})</p>
 </template>
