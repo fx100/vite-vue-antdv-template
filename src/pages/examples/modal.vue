e6872d79e383671a138d50d348d5ccb7f64003bc
shafucheng <flynn.sha@outlook.com>
2022-01-19 12:41:51 +0800

    update: modal title

--- src/pages/examples/modal.vue
+++ src/pages/examples/modal.vue
@@ -4,13 +4,13 @@ import useModal from '~/composables/useModal'
 
 const [modalProps, modalShow] = $(
   useModal({
-    title: '弹窗标题',
+    title: 'useModal定义的弹窗标题',
   }),
 )
 
 async function open() {
   try {
-    const res = await modalShow({ title: '弹窗标题' }, async () => {
+    const res = await modalShow({ title: 'show函数传递的标题' }, async () => {
       try {
         // 模拟请求服务器
         return await new Promise((resolve, reject) => {
@@ -37,7 +37,7 @@ async function open() {
 <template>
   <a-button @click="open">打开弹窗</a-button>
   <!-- 可拖拽弹窗 -->
-  <my-modal v-bind="modalProps">
+  <my-modal title="template上的标题" v-bind="modalProps">
     <!-- 引用自己 -->
     <modal />
   </my-modal>
