c62265c810e42e1b47efc742c23066f5f85ce55f
shafucheng <flynn.sha@outlook.com>
2022-01-19 12:22:00 +0800

    feat: useModal example

--- src/pages/examples/modal.vue
+++ src/pages/examples/modal.vue
@@ -1,12 +1,44 @@
 <script setup lang="ts">
-let visible = $ref(false)
+import { message } from 'ant-design-vue/es'
+import useModal from '~/composables/useModal'
+
+const [modalProps, modalShow] = $(
+  useModal({
+    title: '弹窗标题',
+  }),
+)
+
+async function open() {
+  try {
+    const res = await modalShow({ title: '弹窗标题' }, async () => {
+      try {
+        // 模拟请求服务器
+        return await new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (Math.random() < 0.5) {
+              resolve({ code: 0 })
+            } else {
+              reject({ code: 500, message: '服务器内部错误' })
+            }
+          }, 2000)
+        })
+      } catch (err) {
+        message.error(JSON.stringify(err))
+        throw err
+      }
+    })
+    message.success(JSON.stringify(res))
+  } catch (err) {
+    message.warning('Cancel')
+  }
+}
 </script>
 
 <template>
-  <a-button @click="visible = true">打开弹窗</a-button>
-  <my-modal v-model:visible="visible" title="弹窗 固定高度：60%" height="60%">
-    <p v-for="i in 99" :key="i" style="white-space: nowrap">
-      {{ `${i}`.repeat(50) }}
-    </p>
+  <a-button @click="open">打开弹窗</a-button>
+  <!-- 可拖拽弹窗 -->
+  <my-modal v-bind="modalProps">
+    <!-- 引用自己 -->
+    <modal />
   </my-modal>
 </template>
