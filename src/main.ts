863fd51d79728d3943a73097d530637754492d73
shafucheng <flynn.sha@outlook.com>
2022-01-19 12:19:09 +0800

    全局引入 ant-design-vue 动态创建的组件 css

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import router from './router'
 import App from './App.vue'
+import 'ant-design-vue/es/message/style/css'
+import 'ant-design-vue/es/notification/style/css'
 
 createApp(App).use(router).mount('#app')
