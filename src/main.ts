a135cdfa82974bfc9a36a3cbdb3f5b253bc1238f
shafucheng <flynn.sha@outlook.com>
2022-01-20 18:54:26 +0800

    dayjs add zh-cn

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import router from './router'
 import App from './App.vue'
+
+// dayjs add zh-cn
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+dayjs.locale('zh-cn')
+
 import 'ant-design-vue/es/message/style/css'
 import 'ant-design-vue/es/notification/style/css'
 
