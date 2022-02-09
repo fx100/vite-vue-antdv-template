5c46bf47e011a05c837d695b574850d88f2f0a5a
shafucheng <flynn.sha@outlook.com>
2022-02-09 18:08:38 +0800

    add MyEmpty

---
+++ src/components/My/Empty.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Empty } from 'ant-design-vue/es'
+import type { CSSProperties } from 'vue'
+import type { VueNode } from 'ant-design-vue/es/_util/type'
+
+const props = defineProps<{
+  prefixCls?: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  class?: any
+  style?: string | CSSProperties
+  imageStyle?: CSSProperties
+  image?: VueNode | null
+  description?: VueNode
+}>()
+</script>
+
+<template>
+  <a-empty :image="Empty.PRESENTED_IMAGE_SIMPLE" v-bind="props" />
+</template>
