f12dbec6d71e37095c1a8a04a617f3bbc3124b08
shafucheng <flynn.sha@outlook.com>
2022-02-09 10:45:19 +0800

    add env and d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,14 @@
 /// <reference types="vite/client" />
 /// <reference types="vue/macros-global" />
 
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
