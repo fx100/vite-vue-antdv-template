9ada09864aa1325bf2c419037bd249a1343c5926
shafucheng <flynn.sha@outlook.com>
2022-03-05 03:23:36 +0800

    feat: 扩展 antdv a-empty 组件，默认图片改为 simple

---
+++ src/components/antdv/AEmptySimple.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Empty } from 'ant-design-vue/es'
+import type { CSSProperties } from 'vue'
+import type { VueNode } from 'ant-design-vue/es/_util/type'
+
+interface Props {
+  prefixCls?: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  class?: any
+  style?: string | CSSProperties
+  imageStyle?: CSSProperties
+  image?: VueNode | null
+  description?: VueNode
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <a-empty v-bind="props" :image="Empty.PRESENTED_IMAGE_SIMPLE" />
+</template>
